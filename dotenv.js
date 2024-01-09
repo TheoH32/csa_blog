a8a6ed17f74fe3941da3b454a68b430e701aee97
TheoH32 <theo.h131@gmail.com>
2024-01-08 22:16:21 -0800

    all hacks

---
+++ dotenv.js
@@ -0,0 +1,6 @@
+const dotenv = require( 'dotenv');
+dotenv.config(); // Load variables from .env into process.env
+const apiKey = process.env.API_KEY;
+const dbUrl = process. env.DATABASE_URL;
+console.log('API Key:', apiKey);
+console.log( 'Database URL:', dbUrl);
